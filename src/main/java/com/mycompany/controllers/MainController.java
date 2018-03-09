b40efee37eb33b653edb9e383f3966e748f43dbd
Qiang Ding <qiading@cisco.com>
2018-03-10 00:40:15 +0800

    modify hyperlink for linkedin, github...

--- src/main/java/com/mycompany/controllers/MainController.java
+++ src/main/java/com/mycompany/controllers/MainController.java
@@ -3,11 +3,18 @@ package com.mycompany.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
+
 @Controller
 public class MainController {
 
     @RequestMapping("/")
-    public String index() {
+    public String index(Map<String, Object> model) {
+        model.put("email", "mailto:dqclement@gmail.com");
+        model.put("facebook", "https://www.facebook.com/china.dq");
+        model.put("weibo", "https://www.weibo.com/1671635441");
+        model.put("linkedin", "https://www.linkedin.com/in/qiading");
+        model.put("github", "https://www.github.com/ipodtree");
         return "index";
     }
 
